ac68fbe02f2e6f2c4bba4543ead366f666602e4f
rohit <rohitnainindian@gmail.com>
2025-04-24 00:31:36 +0530

    initial setup for auth with cookies

---
+++ week-16 AuthUsingCookies/backend/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import path from "path";
+
+const JWT_SECRET = "your_jwt_secret_key"; // Replace with your actual secret key
+// const JWT_EXPIRY = "1h"; // Token expiry time
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:5173"
+}));
+
+app.post("/signin", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    // do db validations, fetch id of user from db
+    const token = jwt.sign({
+        id: 1
+    }, JWT_SECRET);
+    res.cookie("token", token);
+    res.send("Logged in!");
+});
+
+app.get("/user", (req, res) => {
+    const token = req.cookies.token;
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    // Get email of the user from the database
+    res.send({
+        userId: decoded.id
+    })
+});
+
+
+app.post("/logout", (req, res) => {
+    res.cookie("token", "ads");
+    res.json({
+        message: "Logged out!"
+    })
+});
+
+
+app.listen(3000);
